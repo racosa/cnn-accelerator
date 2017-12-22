3d4b9b701cbf2a3dfbb3ed1c5ddb6ee311cb07e3
racosa <racosa@protonmail.com>
2017-12-22 01:00:59 +0100

    Parsing kernel coefficients to numpy array format

---
+++ algorithm/cnn/coeff_parser.py
@@ -0,0 +1,13 @@
+import re
+
+input_name = 'coeff.txt'
+output_name = 'coeff.py'
+
+with open(input_name, 'r', encoding='ascii') as input:
+    for line in input:
+        new_line = re.sub(r'\d\s', ',', line)
+        new_line = re.sub(r'[]]\s', '],', new_line)
+        new_line = re.sub(r'[]]]]][,]', ']]]])', new_line)
+        print(new_line)
+        with open(output_name, 'a', encoding='ascii') as output:
+            output.write(new_line+'\n')
