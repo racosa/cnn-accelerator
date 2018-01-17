3d065ed1db9e956e88d37c42fc53052194a068fc
racosa <racosa@protonmail.com>
2018-01-17 11:55:42 +0100

    Adding pre-hls README files

---
+++ pre-hls/fixed-point/README.mk
@@ -0,0 +1,9 @@
+# fixed-point pre-hls
+
+- pre-hls code with fixed-point ac_fixed library.
+
+- make all: to compile and run bin/run-cnn executable.
+
+- ATTENTION: You must always run "bin/run-cnn" from the "pre-hls/fixed-point/" folder, otherwise
+it won't find the image and label files located in "pre-hls/db/" folder.
+
