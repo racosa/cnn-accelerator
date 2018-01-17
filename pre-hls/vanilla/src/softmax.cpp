9a02fcaab6c30f951db6a9cea12e8713d1b9ae96
racosa <racosa@protonmail.com>
2018-01-17 23:22:45 +0100

    Removing c++11 functions from pre-hls/vanilla softmax

--- pre-hls/vanilla/src/softmax.cpp
+++ pre-hls/vanilla/src/softmax.cpp
@@ -1,17 +1,33 @@
 #include "softmax.h"
 
 float softmax(float *input){
-    float max = *std::max_element(input, input+10);
+    float max = input[0];
+    for (int i=1; i<10; i++) {
+        if (max < input[i]) {
+            max = input[i];
+        }
+    }
+ 
     for(int i=0; i<10; i++){
         input[i] = exp(input[i] - max);
     }
 
-    float sum = std::accumulate(input, input+10, 0);
-
+    float sum = input[0];
+    for(int i=1; i<10; i++){
+        sum += input[i];
+    }
+    
     for(int i=0; i<10; i++){
         input[i] = input[i]/sum;
     }
-
-    float max_index = std::distance(input, std::max_element(input, input+10));
+    
+    max = input[0];
+    float max_index = 0;
+    for (int i=1; i<10; i++) {
+        if (max < input[i]) {
+            max = input[i];
+            max_index = i;
+        }
+    }
     return max_index;
 }
