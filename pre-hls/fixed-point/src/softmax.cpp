0ae0bd45443c711e35058ba6605033c0d84534a2
racosa <racosa@protonmail.com>
2018-01-17 11:36:31 +0100

    Removing softmax c++11 functions

--- pre-hls/fixed-point/src/softmax.cpp
+++ pre-hls/fixed-point/src/softmax.cpp
@@ -1,19 +1,34 @@
 #include "softmax.h"
 
 float softmax(double *input){
-    double max = *std::max_element(input, input+10);
-    
+    double max = input[0];
+    for (int i=1; i<10; i++) {
+        if (max < input[i]) {
+            max = input[i];
+        }
+    }
+ 
     for(int i=0; i<10; i++){
         input[i] = exp(input[i] - max);
     }
-    
-    double sum = std::accumulate(input, input+10, 0);
+
+    double sum = input[0];
+    for(int i=1; i<10; i++){
+        sum += input[i];
+    }
     
     for(int i=0; i<10; i++){
         input[i] = input[i]/sum;
     }
 
-    float max_index = std::distance(input, std::max_element(input, input+10));
-
+    max = input[0];
+    float max_index = 0;
+    for (int i=1; i<10; i++) {
+        if (max < input[i]) {
+            max = input[i];
+            max_index = i;
+        }
+    }
+    
     return max_index;
 }
